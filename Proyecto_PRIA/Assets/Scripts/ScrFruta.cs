9ba55db0d6b33f1015edf5957205617927e57692
Escuare <rafaescuare@gmail.com>
2024-01-17 10:29:40 +0100

    Spawn Frutas con tiempo

--- Proyecto_PRIA/Assets/Scripts/ScrFruta.cs
+++ Proyecto_PRIA/Assets/Scripts/ScrFruta.cs
@@ -3,18 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ScrFruta : MonoBehaviour
 {
     public String nombreFruta;
     public float rotateSpeed = 0.1f;
-    private float swingSpeed = 1f;
+    //private float swingSpeed = 1f;
     public AnimationCurve myCurve;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(tiempoVida());
     }
 
     // Update is called once per frame
@@ -26,6 +27,13 @@ public class ScrFruta : MonoBehaviour
         transform.rotation = Quaternion.Euler(-90, rotateSpeed, 0);
         transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
 
+    }
+
+    IEnumerator tiempoVida()
+    {
+        float tiempoRandom = Random.Range(5, 8);
+        yield return new WaitForSeconds(tiempoRandom);
+        Destroy(gameObject);
     }
 
 }
--- Proyecto_PRIA/Assets/Scripts/SpawnFrutas.cs
+++ Proyecto_PRIA/Assets/Scripts/SpawnFrutas.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnFrutas : MonoBehaviour
 {
 
     public GameObject[] Frutas;
-    private int maxFrutas = 5;
     private bool canSpawn = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canSpawn && GameObject.FindGameObjectsWithTag("Fruta").Length<5)
+        if (canSpawn && GameObject.FindGameObjectsWithTag("Fruta").Length < 5)
             StartCoroutine(SpawnCountdownRoutine());
     }
 
@@ -28,22 +28,52 @@ public class SpawnFrutas : MonoBehaviour
     private void SpawnFruta()
     {
         GameObject frutita = Frutas[Random.Range(0, 9)];
-        Instantiate(frutita, GenerateSpawnPosition(), frutita.transform.rotation);
+        Vector3 randomPos = GenerateSpawnPosition();
+
+        if (PosicionOcupada(randomPos))
+        {
+            Debug.Log("Posición ocupada");
+            SpawnFruta();
+        } else
+        {
+            Instantiate(frutita, randomPos, frutita.transform.rotation);
+        }
+     
     }
 
     private Vector3 GenerateSpawnPosition()
     {
+
+        Vector3 randomPos; 
         float spawnPosX = Random.Range(-10, 10);
         float spawnPosZ = Random.Range(-7, 7);
-        Vector3 randomPos = new Vector3(spawnPosX, 1, spawnPosZ);
+        randomPos = new Vector3(spawnPosX, 1, spawnPosZ);
+
         return randomPos;
+    }
+
+    private bool PosicionOcupada(Vector3 randomSpawnPos)
+    {
+        float radius = 1.5f;
+
+        Collider[] colliders = Physics.OverlapSphere(randomSpawnPos, radius);
+
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Fruta"))
+            {
+                return true; //POSICIÓN OCUPADAS
+            }
+        }
+
+        return false;
     }
 
     #endregion
 
     IEnumerator SpawnCountdownRoutine()
     {
-        canSpawn = false; //PARA QUE EN EL UPDATE NO ENTRE, ESPERE 2 SEGUNDOS Y LUEGO ENTRE
+        canSpawn = false; //PARA QUE EN EL UPDATE NO ENTRE, ESPERE Y LUEGO ENTRE
         yield return new WaitForSeconds(1);
         canSpawn = true;
         SpawnFruta();
